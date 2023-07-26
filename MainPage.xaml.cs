e5d25b71bcc49cb3a7a918b164d4d045cababc50
jaximuss <chidiumeo@gmail.com>
2023-07-26 15:52:49 +0100

    remaining the API key

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,57 @@
-﻿namespace WeatherApp;
+﻿using System.Text.Json;
+
+namespace WeatherApp;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
 
-	public MainPage()
+    private WeatherData weatherData;
+
+    // Event handler for the button click to fetch weather data.
+    private async void OnGetWeatherClicked(object sender, EventArgs e)
+    {
+        string apiKey = "YOUR_API_KEY"; // Replace with your OpenWeatherMap API key
+        string cityOrZipCode = cityEntry.Text; // Get the city name or ZIP code from the entry field
+
+        // Fetch weather data from the API
+        weatherData = await GetWeatherDataAsync(cityOrZipCode, apiKey);
+
+        // Update the UI with weather information
+        if (weatherData != null)
+        {
+            cityNameLabel.Text = weatherData.Name;
+            temperatureLabel.Text = $"{weatherData.Main.Temp}°C";
+            descriptionLabel.Text = weatherData.Weather[0].Description;
+        }
+        else
+        {
+            cityNameLabel.Text = "City not found";
+            temperatureLabel.Text = "";
+            descriptionLabel.Text = "";
+        }
+    }
+    public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private async Task<WeatherData> GetWeatherDataAsync(string cityOrZipCode, string apiKey)
+    {
+        using (var httpClient = new HttpClient())
+        {
+            string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={cityOrZipCode}&appid={apiKey}&units=metric";
+            var response = await httpClient.GetAsync(apiUrl);
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var weatherData = JsonSerializer.Deserialize<WeatherData>(content);
+                return weatherData;
+            }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+            return null;
+        }
+    }
 }
 
